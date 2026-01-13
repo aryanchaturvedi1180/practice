80dd176d791a48ee2c0ef855e0667593f02701d5
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-01-13 16:14:59 +0530

    [Rishabh] merge

---
+++ rishabh/cp1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class cp1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int sum =0;
+        for(char ch:input.toCharArray()){
+            sum+=ch-'0';
+        }
+        int a=0;
+        int b=1;
+        while(b<sum){
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        if(b==sum){
+        System.out.println(input+sum);}
+        else{
+            System.out.println(input+b);
+
+        }
+    }
+    
+}
